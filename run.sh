563b2e5d8b18bccf3a9e8cb8a36865bac4835d94
Emmanuel Awa <eawa@brandeis.edu>
2015-11-17 21:11:41 -0500

    Renamed my module to match python module naming convention

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-# example of the run script for running the word count
 
-# I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
-python ./src/words_tweeted.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt
-python ./src/median_unique.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
 
+# I'll execute my programs, with only the program file and input file. Output is coded inside for simplicity, as it's not likely to change
+
+python ./src/solution.py ./tweet_input/tweets.txt
 
 
